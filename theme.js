9da00c27fa6024210d44834944a1dcca4ac129bf
AdobadoDev <adobadodev@users.noreply.github.com>
2026-09-06 01:52:54 +0800

    Fix dark mode issue, enhance project cards, add SEO and competencies

--- theme.js
+++ theme.js
@@ -9,7 +9,7 @@
       return storedTheme;
     }
 
-    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    return "light";
   }
 
   function applyTheme(theme) {
